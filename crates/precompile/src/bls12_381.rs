5dee4e66a8ee57dc74ca131f346716a45ad24093
Federico Gimenez <federico.gimenez@gmail.com>
2024-03-18 12:59:37 +0100

    add g1_add test and first failing case

--- crates/precompile/src/bls12_381.rs
+++ crates/precompile/src/bls12_381.rs
@@ -175,4 +175,30 @@ fn map_fp2_to_g2_impl(_input: &[u8]) -> Option<()> {
 }
 
 #[cfg(test)]
-mod test {}
+mod test {
+    use super::*;
+    use revm_primitives::hex::FromHex;
+    use rstest::rstest;
+
+    #[rstest]
+    // test vectors from https://github.com/ethereum/go-ethereum/blob/master/core/vm/testdata/precompiles/blsG1Add.json and https://github.com/ethereum/go-ethereum/blob/master/core/vm/testdata/precompiles/fail-blsG1Add.json
+    #[case::g1_add_g1_plus_g1_equals_2_times_g1("0000000000000000000000000000000017f1d3a73197d7942695638c4fa9ac0fc3688c4f9774b905a14e3a3f171bac586c55e83ff97a1aeffb3af00adb22c6bb0000000000000000000000000000000008b3f481e3aaa0f1a09e30ed741d8ae4fcf5e095d5d00af600db18cb2c04b3edd03cc744a2888ae40caa232946c5e7e10000000000000000000000000000000017f1d3a73197d7942695638c4fa9ac0fc3688c4f9774b905a14e3a3f171bac586c55e83ff97a1aeffb3af00adb22c6bb0000000000000000000000000000000008b3f481e3aaa0f1a09e30ed741d8ae4fcf5e095d5d00af600db18cb2c04b3edd03cc744a2888ae40caa232946c5e7e1", "000000000000000000000000000000000572cbea904d67468808c8eb50a9450c9721db309128012543902d0ac358a62ae28f75bb8f1c7c42c39a8c5529bf0f4e00000000000000000000000000000000166a9d8cabc673a322fda673779d8e3822ba3ecb8670e461f73bb9021d5fd76a4c56d9d4cd16bd1bba86881979749d28", false, 500)]
+    fn test_g1_add(
+        #[case] input: &str,
+        #[case] expected_output: &str,
+        #[case] expected_error: bool,
+        #[case] expected_gas: u64,
+    ) {
+        let input = Bytes::from_hex(input).unwrap();
+        let target_gas: u64 = 30_000_000;
+        let res = g1_add(&input, target_gas);
+        if expected_error {
+            assert!(res.is_err());
+        } else {
+            let (actual_gas, actual_output) = res.unwrap();
+            assert_eq!(expected_gas, actual_gas);
+            let expected_output = Bytes::from_hex(expected_output).unwrap();
+            assert_eq!(expected_output, actual_output);
+        }
+    }
+}
