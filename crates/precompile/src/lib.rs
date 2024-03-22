7f70f293ae323e5c58291ec7333e55ed67489d2d
Federico Gimenez <federico.gimenez@gmail.com>
2024-03-22 10:29:48 +0100

    use revm_precompile::u64_to_address

--- crates/precompile/src/lib.rs
+++ crates/precompile/src/lib.rs
@@ -11,15 +11,3 @@ pub mod secp256r1;
 pub mod bls12_381;
 
 mod addresses;
-
-/// Const function for making an address by concatenating the bytes from two given numbers.
-///
-/// Note that 32 + 128 = 160 = 20 bytes (the length of an address). This function is used
-/// as a convenience for specifying the addresses of the various precompiles.
-#[inline]
-const fn u64_to_address(x: u64) -> Address {
-    let x = x.to_be_bytes();
-    Address::new([
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, x[0], x[1], x[2], x[3], x[4], x[5], x[6], x[7],
-    ])
-}
