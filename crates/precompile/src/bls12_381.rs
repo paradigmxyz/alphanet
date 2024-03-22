9ca2468097380595d06385e0ad17e6544cbbae52
Federico Gimenez <federico.gimenez@gmail.com>
2024-03-22 14:59:48 +0100

    removed leftovers

--- crates/precompile/src/bls12_381.rs
+++ crates/precompile/src/bls12_381.rs
@@ -33,31 +33,6 @@ pub fn precompiles() -> impl Iterator<Item = PrecompileWithAddress> {
     .into_iter()
 }
 
-const G1ADD_BASE: u64 = 500;
-const INPUT_LENGTH: usize = 256;
-const INPUT_ITEM_LENGTH: usize = 128;
-const OUTPUT_LENGTH: usize = 128;
-const FP_LENGTH: usize = 48;
-const PADDED_INPUT_LENGTH: usize = 64;
-const PADDING_LEGTH: usize = 16;
-const FP_CONCAT_LENGTH: usize = 96;
-
-/// bls12381 precompiles
-pub fn precompiles() -> impl Iterator<Item = PrecompileWithAddress> {
-    [
-        BLS12_G1ADD,
-        BLS12_G1MUL,
-        BLS12_G1MULTIEXP,
-        BLS12_G2ADD,
-        BLS12_G2MUL,
-        BLS12_G2MULTIEXP,
-        BLS12_PAIRING,
-        BLS12_MAP_FP_TO_G1,
-        BLS12_MAP_FP2_TO_G2,
-    ]
-    .into_iter()
-}
-
 /// [EIP-2537](https://eips.ethereum.org/EIPS/eip-2537#specification) BLS12_G1ADD precompile.
 const BLS12_G1ADD: PrecompileWithAddress =
     PrecompileWithAddress(u64_to_address(BLS12_G1ADD_ADDRESS), Precompile::Standard(g1_add));
@@ -140,20 +115,6 @@ fn extract_g1_input(input: &[u8]) -> Result<G1Affine, PrecompileError> {
         }
         Ok(output.unwrap())
     }
-
-    let input_p0_x = match remove_padding(&input[..PADDED_INPUT_LENGTH]) {
-        Ok(input_p0_x) => input_p0_x,
-        Err(e) => return Err(e),
-    };
-    let input_p0_y = match remove_padding(&input[PADDED_INPUT_LENGTH..INPUT_ITEM_LENGTH]) {
-        Ok(input_p0_y) => input_p0_y,
-        Err(e) => return Err(e),
-    };
-    let mut input_p0: [u8; FP_CONCAT_LENGTH] = [0; FP_CONCAT_LENGTH];
-    input_p0[..FP_LEGTH].copy_from_slice(&input_p0_x);
-    input_p0[FP_LEGTH..].copy_from_slice(&input_p0_y);
-
-    Ok(input_p0)
 }
 
 // G1 addition call expects `256` bytes as an input that is interpreted as byte
@@ -295,125 +256,6 @@ fn map_fp_to_g1(_input: &Bytes, gas_limit: u64) -> PrecompileResult {
 /// [EIP-2537](https://eips.ethereum.org/EIPS/eip-2537#specification) BLS12_MAP_FP2_TO_G2 precompile.
 const BLS12_MAP_FP2_TO_G2: PrecompileWithAddress = PrecompileWithAddress(
     u64_to_address(BLS12_MAP_FP2_TO_G2_ADDRESS),
-    Precompile::Standard(map_fp2_to_g2),
-);
-
-fn map_fp2_to_g2(_input: &Bytes, gas_limit: u64) -> PrecompileResult {
-    // TODO: make gas base depend on input k
-    const MAP_FP2_TO_G2_BASE: u64 = 12000;
-    if MAP_FP2_TO_G2_BASE > gas_limit {
-        return Err(PrecompileError::OutOfGas);
-    }
-    let result = 1;
-    Ok((MAP_FP2_TO_G2_BASE, B256::with_last_byte(result as u8).into()))
-}
-
-/// [EIP-2537](https://eips.ethereum.org/EIPS/eip-2537#specification) BLS12_G1MUL precompile.
-pub const BLS12_G1MUL: PrecompileWithAddress =
-    PrecompileWithAddress(crate::u64_to_address(BLS12_G1MUL_ADDRESS), Precompile::Standard(g1_mul));
-
-fn g1_mul(_input: &Bytes, gas_limit: u64) -> PrecompileResult {
-    const G1MUL_BASE: u64 = 12000;
-    if G1MUL_BASE > gas_limit {
-        return Err(PrecompileError::OutOfGas);
-    }
-    let result = 1;
-    Ok((G1MUL_BASE, B256::with_last_byte(result as u8).into()))
-}
-
-/// [EIP-2537](https://eips.ethereum.org/EIPS/eip-2537#specification) BLS12_G1MULTIEXP precompile.
-const BLS12_G1MULTIEXP: PrecompileWithAddress = PrecompileWithAddress(
-    crate::u64_to_address(BLS12_G1MULTIEXP_ADDRESS),
-    Precompile::Standard(g1_multiexp),
-);
-
-fn g1_multiexp(_input: &Bytes, gas_limit: u64) -> PrecompileResult {
-    // TODO: make gas base depend on input k
-    const G1MULTIEXP_BASE: u64 = 12000;
-    if G1MULTIEXP_BASE > gas_limit {
-        return Err(PrecompileError::OutOfGas);
-    }
-    let result = 1;
-    Ok((G1MULTIEXP_BASE, B256::with_last_byte(result as u8).into()))
-}
-
-/// [EIP-2537](https://eips.ethereum.org/EIPS/eip-2537#specification) BLS12_G2ADD precompile.
-const BLS12_G2ADD: PrecompileWithAddress =
-    PrecompileWithAddress(crate::u64_to_address(BLS12_G2ADD_ADDRESS), Precompile::Standard(g2_add));
-
-fn g2_add(_input: &Bytes, gas_limit: u64) -> PrecompileResult {
-    const G2ADD_BASE: u64 = 800;
-    if G2ADD_BASE > gas_limit {
-        return Err(PrecompileError::OutOfGas);
-    }
-    let result = 1;
-    Ok((G2ADD_BASE, B256::with_last_byte(result as u8).into()))
-}
-
-/// [EIP-2537](https://eips.ethereum.org/EIPS/eip-2537#specification) BLS12_G2MUL precompile.
-const BLS12_G2MUL: PrecompileWithAddress =
-    PrecompileWithAddress(crate::u64_to_address(BLS12_G2MUL_ADDRESS), Precompile::Standard(g2_mul));
-
-fn g2_mul(_input: &Bytes, gas_limit: u64) -> PrecompileResult {
-    const G2MUL_BASE: u64 = 45000;
-    if G2MUL_BASE > gas_limit {
-        return Err(PrecompileError::OutOfGas);
-    }
-    let result = 1;
-    Ok((G2MUL_BASE, B256::with_last_byte(result as u8).into()))
-}
-
-/// [EIP-2537](https://eips.ethereum.org/EIPS/eip-2537#specification) BLS12_G2MULTIEXP precompile.
-const BLS12_G2MULTIEXP: PrecompileWithAddress = PrecompileWithAddress(
-    crate::u64_to_address(BLS12_G2MULTIEXP_ADDRESS),
-    Precompile::Standard(g2_multiexp),
-);
-
-fn g2_multiexp(_input: &Bytes, gas_limit: u64) -> PrecompileResult {
-    // TODO: make gas base depend on input k
-    const G2MULTIEXP_BASE: u64 = 12000;
-    if G2MULTIEXP_BASE > gas_limit {
-        return Err(PrecompileError::OutOfGas);
-    }
-    let result = 1;
-    Ok((G2MULTIEXP_BASE, B256::with_last_byte(result as u8).into()))
-}
-
-/// [EIP-2537](https://eips.ethereum.org/EIPS/eip-2537#specification) BLS12_PAIRING precompile.
-const BLS12_PAIRING: PrecompileWithAddress = PrecompileWithAddress(
-    crate::u64_to_address(BLS12_PAIRING_ADDRESS),
-    Precompile::Standard(pairing),
-);
-
-fn pairing(_input: &Bytes, gas_limit: u64) -> PrecompileResult {
-    // TODO: make gas base depend on input k
-    const PAIRING_BASE: u64 = 12000;
-    if PAIRING_BASE > gas_limit {
-        return Err(PrecompileError::OutOfGas);
-    }
-    let result = 1;
-    Ok((PAIRING_BASE, B256::with_last_byte(result as u8).into()))
-}
-
-/// [EIP-2537](https://eips.ethereum.org/EIPS/eip-2537#specification) BLS12_MAP_FP_TO_G1 precompile.
-const BLS12_MAP_FP_TO_G1: PrecompileWithAddress = PrecompileWithAddress(
-    crate::u64_to_address(BLS12_MAP_FP_TO_G1_ADDRESS),
-    Precompile::Standard(map_fp_to_g1),
-);
-
-fn map_fp_to_g1(_input: &Bytes, gas_limit: u64) -> PrecompileResult {
-    // TODO: make gas base depend on input k
-    const MAP_FP_TO_G1_BASE: u64 = 12000;
-    if MAP_FP_TO_G1_BASE > gas_limit {
-        return Err(PrecompileError::OutOfGas);
-    }
-    let result = 1;
-    Ok((MAP_FP_TO_G1_BASE, B256::with_last_byte(result as u8).into()))
-}
-
-/// [EIP-2537](https://eips.ethereum.org/EIPS/eip-2537#specification) BLS12_MAP_FP2_TO_G2 precompile.
-const BLS12_MAP_FP2_TO_G2: PrecompileWithAddress = PrecompileWithAddress(
-    crate::u64_to_address(BLS12_MAP_FP2_TO_G2_ADDRESS),
     Precompile::Standard(map_fp2_to_g2),
 );
 
