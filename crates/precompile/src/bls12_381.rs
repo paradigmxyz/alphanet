9228a41f6d7b9fcec27725c1ab58e5935292b805
Federico Gimenez <federico.gimenez@gmail.com>
2024-04-04 19:48:50 +0200

    typo

--- crates/precompile/src/bls12_381.rs
+++ crates/precompile/src/bls12_381.rs
@@ -494,7 +494,7 @@ const BLS12_G2MULTIEXP: PrecompileWithAddress = PrecompileWithAddress(
 /// Implements EIP-2537 G2MultiExp precompile.
 /// G2 multiplication call expects `288*k` bytes as an input that is interpreted
 /// as byte concatenation of `k` slices each of them being a byte concatenation
-/// of encoding of G1 point (`256` bytes) and encoding of a scalar value (`32`
+/// of encoding of G2 point (`256` bytes) and encoding of a scalar value (`32`
 /// bytes).
 /// Output is an encoding of multiexponentiation operation result - single G2
 /// point (`256` bytes). See also:
