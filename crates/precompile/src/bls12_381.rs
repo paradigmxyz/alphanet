57a306a8d755e622b8a57d55e9f98f53dc9a8545
Federico Gimenez <federico.gimenez@gmail.com>
2024-05-30 09:23:51 +0200

    chore: remove revm dependency

--- crates/precompile/src/bls12_381.rs
+++ crates/precompile/src/bls12_381.rs
@@ -1,14 +1,15 @@
 //! # EIP-2537 BLS12-381 Precompiles imported from Revm.
 
-use revm::precompile::bls12_381::{
-    g1_add::PRECOMPILE as BLS12_G1ADD, g1_msm::PRECOMPILE as BLS12_G1MSM,
-    g1_mul::PRECOMPILE as BLS12_G1MUL, g2_add::PRECOMPILE as BLS12_G2ADD,
-    g2_msm::PRECOMPILE as BLS12_G2MSM, g2_mul::PRECOMPILE as BLS12_G2MUL,
-    map_fp2_to_g2::PRECOMPILE as BLS12_MAP_FP2_TO_G2,
-    map_fp_to_g1::PRECOMPILE as BLS12_MAP_FP_TO_G1, pairing::PRECOMPILE as BLS12_PAIRING,
+use reth::revm::precompile::{
+    bls12_381::{
+        g1_add::PRECOMPILE as BLS12_G1ADD, g1_msm::PRECOMPILE as BLS12_G1MSM,
+        g1_mul::PRECOMPILE as BLS12_G1MUL, g2_add::PRECOMPILE as BLS12_G2ADD,
+        g2_msm::PRECOMPILE as BLS12_G2MSM, g2_mul::PRECOMPILE as BLS12_G2MUL,
+        map_fp2_to_g2::PRECOMPILE as BLS12_MAP_FP2_TO_G2,
+        map_fp_to_g1::PRECOMPILE as BLS12_MAP_FP_TO_G1, pairing::PRECOMPILE as BLS12_PAIRING,
+    },
+    PrecompileWithAddress,
 };
-
-use reth::revm::precompile::PrecompileWithAddress;
 
 /// Returns the bls12381 precompiles with their addresses.
 pub fn precompiles() -> impl Iterator<Item = PrecompileWithAddress> {
