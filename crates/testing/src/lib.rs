2676bed789d496418f8f09a3a3cf0e74bd03e819
Oliver Nordbjerg <hi@notbjerg.me>
2024-08-20 08:36:17 +0200

    chore: temp allow unused deps

--- crates/testing/src/lib.rs
+++ crates/testing/src/lib.rs
@@ -1,6 +1,9 @@
 //! Alphanet-testing module
 
-#![warn(unused_crate_dependencies)]
+// todo(onbjerg): uncomment this when adding a test,
+// it was commented out due to removing our bls tests since they are now
+// in revm
+// #![warn(unused_crate_dependencies)]
 
 #[cfg(test)]
 mod test_suite;
