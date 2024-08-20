32506c2d273d06bba25fea12f6032e72c064c74b
Oliver <onbjerg@users.noreply.github.com>
2024-08-20 17:53:55 +0200

    chore: remove bls precompiles (#101)
    * chore: remove bls precompiles
    
    * chore: temp allow unused deps
    
    * chore: allow dead code for test crate
    
    * chore: enable prague by default

--- crates/precompile/src/lib.rs
+++ crates/precompile/src/lib.rs
@@ -3,15 +3,10 @@
 //! Implementations of EVM precompiled contracts for AlphaNet.
 //!
 //! Alphanet currently implements the following EIPs, which define precompiles:
-//! - [EIP-2537](https://eips.ethereum.org/EIPS/eip-2537): Precompile for BLS12-381 curve
-//!   operations. The precompile implementation is located in the [bls12_381] module.
 //! - [EIP-7212](https://eips.ethereum.org/EIPS/eip-7212): Precompile for secp256r1 Curve Support.
 //!   The precompile implementation is located in the [secp256r1] module.
 
 #![cfg_attr(not(test), warn(unused_crate_dependencies))]
-
-/// The implementation of [EIP-2537](https://eips.ethereum.org/EIPS/eip-2537): Precompile for BLS12-381 curve.
-pub mod bls12_381;
 
 /// The implementation of [EIP-7212](https://eips.ethereum.org/EIPS/eip-7212): Precompile for secp256r1 Curve Support.
 pub mod secp256r1;
